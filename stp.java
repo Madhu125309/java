5b63d8e1fa8ddd0295351deea1c40bc5275ffe71
7997259422 <madhureddy85767@gmail.com>
2023-06-08 16:04:34 +0530

    hi

---
+++ stp.java
@@ -0,0 +1,14 @@
+public class stp {
+    public static void main(String[] args) {
+        String first = "Madhu";
+        String last = "sudhan reddy";
+        String fullname = first + last;
+        int x = 10;
+        int y = 5;
+
+        int a = 10, b = 15, c = 20, d = 12;
+        System.out.println(fullname);
+        System.out.println(x + y);
+        System.out.println(a * b + c - d);
+    }
+}
---
+++ two2.java
@@ -0,0 +1,10 @@
+public class two2 {
+
+    public static void main(String args[]) {
+        int myint = 9;
+        double mydouble = myint; // automatic casting:int to double
+
+        System.out.println(myint); // outputs 9
+        System.out.println(mydouble); // outputs 9.0
+    }
+}
