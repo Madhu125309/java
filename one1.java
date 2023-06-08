9d4c9c41ed121ee0265f128a4467de1a556a824f
7997259422 <madhureddy85767@gmail.com>
2023-06-08 12:48:27 +0530

    hi

---
+++ one1.java
@@ -0,0 +1,23 @@
+
+class one1 {
+    public static void main(String args[]) {
+        byte a = 100;
+        short b = 589;
+        int c = 1200;
+        long d = 7997259422l;
+        float e = 45700.22f;
+        double f = 47520.11;
+        char g = 'm';
+        boolean h = true;
+        String i = "madhu";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+    }
+}
