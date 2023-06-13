00472a40ebd05012ca7f82ecb7b257cb3e22faa5
7997259422 <madhureddy85767@gmail.com>
2023-06-13 13:47:38 +0530

    hey

---
+++ test4.java
@@ -0,0 +1,9 @@
+public class test4 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = a++; // post increment first assin then incr
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
