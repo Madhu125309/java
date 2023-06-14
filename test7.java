bcda353e77b6af084f88c9b7772815ed49400550
7997259422 <madhureddy85767@gmail.com>
2023-06-14 13:47:13 +0530

    hai

---
+++ test7.java
@@ -0,0 +1,12 @@
+public class test7 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println(a + b);
+        System.out.println(a * b);
+        System.out.println(a - b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+
+}
