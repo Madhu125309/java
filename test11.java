0596bde5aebea78ce75606f9caa8294bcb4802cc
7997259422 <madhureddy85767@gmail.com>
2023-06-14 13:46:52 +0530

    hai

---
+++ test11.java
@@ -0,0 +1,8 @@
+public class test11 {
+    public static void main(String[] args) {
+        int ids[] = { 101, 102, 103, 104 }; // for each
+        for (int id : ids)
+
+            System.out.println(id);
+    }
+}
